57d3f4f0bdbf817e910b8f0bd2778c1ce891f0c8
Makeademic <kaplanwizard@gmail.com>
2025-05-08 10:28:26 -0400

    Update search-ui.js

--- assets/search-ui.js
+++ assets/search-ui.js
@@ -1,7 +1,3 @@
----
----
-
-
 // Methods and jQuery UI for Wax search box
 function excerptedString(str) {
   str = str || ''; // handle null > string
@@ -13,20 +9,49 @@ function excerptedString(str) {
   }
 }
 
+function getThumbnail(item, url) {
+  if ('thumbnail' in item) {
+    return `<img class='sq-thumb-sm' src='${url}${item.thumbnail}'/>&nbsp;&nbsp;&nbsp;`
+  }
+  else {
+    return '';
+  }
+  console.log("returned thumbnail")
+}
 
 function displayResult(item, fields, url) {
-  var pid   = item.pid;
-  var title = item.title || 'Untitled';
-  var link  = item.permalink.toLowerCase();
+  console.log("display thumbnail")
+  var transtitle = item.transtitle;
+  var title = item.title;
+  var link  = item.permalink;
+  var author = item.author;
+  var translator = item.translator;
   var meta  = []
 
+
   for (i in fields) {
-    fieldTitle = fields[i];
-    if (fieldTitle in item ) {
-      meta.push(`<b>${fieldTitle}:</b> ${excerptedString(item[fieldTitle])}`);
+    fieldLabel = fields[i];
+    if (fieldLabel in item ) {
+      if (fieldLabel !== "title" && fieldLabel !== "transtitle" && fieldLabel !== "poem" && fieldLabel !== "transpoem") {
+      meta.push(`<br>${excerptedString(item[fieldLabel])}`);
+      }
     }
   }
-  return `<div class="result"><a href="${url}${link}"><p><span class="title">${item.title}</span><br><span class="meta">${meta.join(' | ')}</span></p></a></div>`;
+
+  if (title != null && transtitle != null){
+
+    return `<div class="result"><a href="${url}${link}"><p><b><span class="title">${item.title}</span></b><br><i><span class="title">${item.transtitle}</span></i><span class="meta">${meta.join('')}</span></p></a></div>`;
+
+  }else if (title == null && transtitle != null){
+
+    return `<div class="result"><a href="${url}${link}"><p><b><span class="title">${item.transtitle}</span></b><span class="meta">${meta.join('')}</span></p></a></div>`;
+
+  }else if (title != null && transtitle == null){
+
+    return `<div class="result"><a href="${url}${link}"><p><b><span class="title">${item.title}</span></b><span class="meta">${meta.join('')}</span></p></a></div>`;
+
+  }
+
 }
 
 function startSearchUI(fields, indexFile, url) {
@@ -35,14 +60,15 @@ function startSearchUI(fields, indexFile, url) {
 
     index.saveDocument(false);
     index.setRef('lunr_id');
-
+    console.log("started search UI")
     for (i in fields) { index.addField(fields[i]); }
     for (i in store)  { index.addDoc(store[i]); }
 
     $('input#search').on('keyup', function() {
+      console.log("getting results")
       var results_div = $('#results');
       var query       = $(this).val();
-      var results     = index.search(query, { bool: 'AND', expand: true });
+      var results     = index.search(query, { boolean: 'AND', expand: true });
 
       results_div.empty();
       results_div.append(`<p class="results-info">Displaying ${results.length} results</p>`);
@@ -51,7 +77,7 @@ function startSearchUI(fields, indexFile, url) {
         var ref    = results[r].ref;
         var item   = store[ref];
         var result = displayResult(item, fields, url);
-
+        console.log(item);
         results_div.append(result);
       }
     });
